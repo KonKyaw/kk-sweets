040fad636780d7e89adbebfdade10531ee26601a
KonKyaw <kaungkyaw1994@gmail.com>
2025-08-17 15:34:10 +0900

    feature: added paginator and category fliter for products page - adjusted navbar component - fixed dashboard search box to search different language titles respective to active language

--- src/app/top-page/products/products.component.ts
+++ src/app/top-page/products/products.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ProductService } from 'shared/services/product.service';
 import { ActivatedRoute } from '@angular/router';
 import { AppProduct } from 'shared/models/app-product';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-products',
@@ -13,6 +15,8 @@ export class ProductsComponent {
   filteredProducts: any[] = []; //cannot use interface cos doesn't have key
   // products$: Observable<any> = new Observable
   category: string | null = '';
+  dataSource = new MatTableDataSource<any>([]);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(route: ActivatedRoute, productService: ProductService) {
     productService.getAll().subscribe((products: Array<AppProduct>) => {
@@ -21,20 +25,23 @@ export class ProductsComponent {
 
       //needs refactoring with switchMap
       //Dealing with Multiple Asynchronous Operations
+      
       route.queryParamMap.subscribe((params) => {
         this.category = params.get('category');
 
-        if (this.category) {
-          this.category = this.category.toLowerCase();
-        }
+        // Filter & sort
+        this.filteredProducts = this.products
+          .filter((p) => p.isActive === true)
+          .filter((p) => !this.category || p.category.toLowerCase() === this.category.toLowerCase())
+          .sort((a: AppProduct, b: AppProduct) => a.order - b.order);
 
-        this.filteredProducts = this.category
-          ? this.products.filter(
-              (p) => p.category.toLowerCase() === this.category
-            )
-          : this.products;
-        this.filteredProducts = this.filteredProducts.sort(((a:AppProduct,b:AppProduct) => a.order - b.order));
+        // Pass filtered array into dataSource
+        this.dataSource.data = this.filteredProducts;
       });
     });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
