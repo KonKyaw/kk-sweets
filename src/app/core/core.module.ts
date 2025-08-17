6f96470e0fc21aab04446c5b9bcd9150db654e08
KonKyaw <kaungkyaw1994@gmail.com>
2025-08-17 15:27:14 +0900

    feature: added NgOptimizedImage for image loading performance - removed stylesheet to solve MIME type error - adjusted style in footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -19,7 +19,8 @@ import { SharedModule } from 'shared/shared.module';
     CommonModule,
     SharedModule,
     CoreRoutingModule,
-    MatIconModule
+    MatIconModule,
+    NgOptimizedImage
   ],
   exports: [
     NavbarComponent,
